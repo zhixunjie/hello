d622b53576394a846533a1908c2c8c7a820258b1
JasonZhi <369610501@qq.com>
2018-08-23 23:52:12 +0800

    init

--- src/HelloClass.php
+++ src/HelloClass.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace Hello;
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 class MyClass
 {
     static function printInfo()
     {
-        $curl = new Curl\Curl();
+        $curl = new \Curl\Curl();
         $curl->get('http://www.example.com/');
         var_dump($curl->response);
     }
